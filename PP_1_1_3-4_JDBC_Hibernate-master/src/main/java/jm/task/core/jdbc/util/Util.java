67c1e7b91554e682786aa401968f7e42ff13026a
stkllo0 <106972900+stkllo0@users.noreply.github.com>
2023-01-26 13:34:16 +0300

    Update Util.java

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,41 +1,21 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.*;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
-    private static final String PASSWORD = "root123.";
-    private static final String USER_NAME = "root";
     private static final String URL = "jdbc:mysql://localhost:3306/mysql";
-    private static Connection connection = null;
-    private static Util instance = null;
-    private Util() {
+    private static final String LOGIN = "root";
+    private static final String PASS = "root123.";
 
+    public static Connection getConnection() {
         try {
-            Driver driver = new com.mysql.cj.jdbc.Driver();
-            DriverManager.registerDriver(driver);
-            System.out.println("Драйвер подключен.");
-        } catch (SQLException e) {
-            System.out.println("error");
+            return DriverManager.getConnection(URL, LOGIN, PASS);
+        } catch (SQLException ex) {
+            System.out.println("Соединение не создано");
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
         }
-        try {
-            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
-            if (!connection.isClosed()) {
-                System.out.println("Соединение установлено.");
-            }
-            Statement statement = connection.createStatement();
-        } catch (SQLException e) {
-            System.out.println("Отсутствует соединение или некорректно создан statement");
-            e.printStackTrace();
-        }
-    }
-    public static Util getInstance() {
-        if (null == instance) {
-            instance = new Util();
-        }
-        return instance;
-    }
-    public static Connection getConnection(){
-        return connection;
     }
 }
