1b34c3c37753a22acbcb38033db27d6e5de7c28f
stkllo0 <106972900+stkllo0@users.noreply.github.com>
2023-01-26 13:36:13 +0300

    Update User.java

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/model/User.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/model/User.java
@@ -19,30 +19,6 @@ public class User {
     @Column
     private Byte age;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName) && Objects.equals(age, user.age);
-    }
-
-    @Override
-    public int hashCode()
-    {
-        int result = 17;
-
-        // 31 используется, поскольку это простое число, а также обеспечивает более высокую производительность, поскольку:
-        // 31 * result == (result << 5) - result
-
-        result = 31 * result + (name == null ? 0 : name.hashCode());
-        result = 31 * result + age;
-        result = 31 * result + (id == null ? 0 : id.hashCode());
-        result = 31 * result + (lastName == null ? 0 : lastName.hashCode());
-
-        return result;
-    }
-
     public User() {
 
     }
@@ -88,6 +64,29 @@ public class User {
     @Override
     public String toString() {
         return String.format("User {id = '%s', name = '%s', lastName = '%s', age = '%d'}", getId(), getName(), getLastName(), getAge());
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName) && Objects.equals(age, user.age);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = 17;
+
+        // 31 используется, поскольку это простое число, а также обеспечивает более высокую производительность, поскольку:
+        // 31 * result == (result << 5) - result
+
+        result = 31 * result + (name == null ? 0 : name.hashCode());
+        result = 31 * result + age;
+        result = 31 * result + (id == null ? 0 : id.hashCode());
+        result = 31 * result + (lastName == null ? 0 : lastName.hashCode());
+
+        return result;
     }
 }
 
