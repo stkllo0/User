3baf5d2be1b4ef87a1d3f50e44fa6d9924527f51
stkllo0 <106972900+stkllo0@users.noreply.github.com>
2023-01-26 13:33:12 +0300

    Update UserDaoJDBCImpl.java

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,10 +2,7 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-
-import javax.swing.text.html.HTMLDocument;
 import java.sql.Statement;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,71 +13,56 @@ import java.util.List;
 
 
 public class UserDaoJDBCImpl implements UserDao {
-    private static final Connection connection = Util.getInstance().getConnection();
     public UserDaoJDBCImpl() {
 
     }
-
+    @Override
     public void createUsersTable() {
-        try(Statement statement = connection.createStatement()){
-            connection.setAutoCommit(false);
+        try(Statement statement = Util.getConnection().createStatement()){
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS User" + "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), lastName VARCHAR(255), age INT)");
-            connection.commit();
-            connection.setAutoCommit(true);
         } catch (SQLException e){
             System.out.println("Таблица не создана");
             e.printStackTrace();
         }
     }
-
+    @Override
     public void dropUsersTable() {
-        try(Statement statement = connection.createStatement()){
-            connection.setAutoCommit(false);
+        try(Statement statement = Util.getConnection().createStatement()){
             statement.executeUpdate("DROP TABLE IF EXISTS User");
-            connection.commit();
-            connection.setAutoCommit(true);
         } catch (SQLException e){
             System.out.println("Не удалось удалить таблицу");
             e.printStackTrace();
         }
     }
-
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         try (PreparedStatement statement =
-                     connection.prepareStatement("INSERT INTO User (name, lastName, age) VALUES (?, ?, ?)")) {
-            connection.setAutoCommit(false);
+                     Util.getConnection().prepareStatement("INSERT INTO User (name, lastName, age) VALUES (?, ?, ?)")) {
             statement.setString(1, name);
             statement.setString(2, lastName);
             statement.setByte(3, age);
             statement.executeUpdate();
-            connection.commit();
-            connection.setAutoCommit(true);
         } catch (SQLException e) {
             System.out.println("При добавлении пользователя произошла ошибка");
             e.printStackTrace();
         }
     }
-
+    @Override
     public void removeUserById(long id){
         try (PreparedStatement statement =
-                     connection.prepareStatement("DELETE FROM User WHERE ID = ?")) {
-            connection.setAutoCommit(false);
+                     Util.getConnection().prepareStatement("DELETE FROM User WHERE ID = ?")) {
             statement.setLong(1, id);
             statement.executeUpdate();
-            connection.commit();
-            connection.setAutoCommit(true);
         } catch (SQLException e) {
             System.out.println("Не удалось удалить пользователя");
             e.printStackTrace();
         }
 
     }
-
+    @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try( PreparedStatement statement = connection.prepareStatement("SELECT * FROM User")){
-            connection.setAutoCommit(false);
-
+        try(PreparedStatement statement = Util.getConnection().prepareStatement("SELECT * FROM User");){
             ResultSet resultSet = statement.executeQuery();
             while(resultSet.next()){
                 User user = new User(resultSet.getString("Name"),
@@ -88,23 +70,16 @@ public class UserDaoJDBCImpl implements UserDao {
                 user.setId(resultSet.getLong("id"));
                 users.add(user);
             }
-            connection.commit();
-            connection.setAutoCommit(true);
         } catch (SQLException e){
             System.out.println("Не удалось получить список");
             e.printStackTrace();
         }
         return users;
-
-
     }
-
+    @Override
     public void cleanUsersTable() {
-        try(Connection connection =Util.getInstance().getConnection()){
-            connection.setAutoCommit(false);
-            PreparedStatement statement = connection.prepareStatement("TRUNCATE TABLE User");
-            connection.commit();
-            connection.setAutoCommit(true);
+        try(PreparedStatement statement = Util.getConnection().prepareStatement("DELETE FROM User")){
+            statement.execute();
         } catch(SQLException e){
             System.out.println("Не удалось очистить таблицу");
             e.printStackTrace();
